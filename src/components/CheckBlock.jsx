ba866457e26c8d8e7daed1e94b1b496fcf4f1d22
Scott Fanetti <scott.fanetti@gmail.com>
2022-08-24 14:19:28 -0400

    adding dependency

---
+++ src/components/CheckBlock.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export default function CheckBlock() {
+    const [checked, setChecked] = useState(false);
+    function getStyle(checked) {
+        return checked ? '0.3rem solid red' : '0.3rem dotted grey';
+    }
+    function toggleChecked() {
+        setChecked(!checked);
+    }
+    return <div 
+            style={{ 
+                border: getStyle(checked), 
+                padding: '2rem 3rem', 
+                margin: '3rem'
+            }}>
+                <h3>The checked value is {checked ? 'true' : 'false'}</h3>
+                <input 
+                 style={{
+                    width: '3rem', 
+                    height: '3rem',
+                    cursor: 'pointer'
+                 }} 
+                 type="checkbox"
+                 value={checked}
+                 onChange={toggleChecked}
+                />
+    </div>
+}
